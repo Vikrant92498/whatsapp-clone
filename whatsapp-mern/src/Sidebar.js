5e7a3176b8e7c26860af73e5a573d3a00bbb51c0
vikrant <vikrantkumar92498@gmail.com>
2023-05-28 16:01:51 +0530

    sidebar done

--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import "./Sidebar.css"
-
+import SidebarChat from "./SidebarChat"
 function sidebar() {
   return (
     <div className='sidebar'>
       <div className="sidebar_header">
         <div className="sidebar_headerRight">
         </div>
+      </div>
+      <div className="sidebar_search">
+        <div className="sidebar_searchContainer">
+          <input type="text" placeholder='Search or start new chat'/>
+        </div>
+      </div>
+      <div className="sidebar_chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
       </div>
     </div> 
   )
