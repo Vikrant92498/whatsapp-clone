ee4e6f322a07edee3eda83b8cdf18ab391365fac
vikrant <vikrantkumar92498@gmail.com>
2023-05-28 13:10:53 +0530

    created chat , Sidebar js and css files

---
+++ whatsapp-mern/src/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Chat.css'
+function chat() {
+  return (
+    <div className='chat'>
+      <h1>I am chat component</h1>
+    </div>
+  )
+}
+
+export default chat
---
+++ whatsapp-mern/src/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Sidebar.css"
+
+function sidebar() {
+  return (
+    <div className='sidebar'>
+      <div className="sidebar_header">
+        <div className="sidebar_headerRight">
+        </div>
+      </div>
+    </div> 
+  )
+} 
+
+export default sidebar
