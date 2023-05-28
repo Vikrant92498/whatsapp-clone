343da98e84011f87a2da4afa2e6097f10b63e50e
vikrant <vikrantkumar92498@gmail.com>
2023-05-28 16:01:14 +0530

    created SidebarChat.css  and SidebarChat.js file

---
+++ whatsapp-mern/src/SidebarChat.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./SidebarChat.css";
+function SidebarChat() {
+  return (
+    <div className='sidebarChat'>
+      <div className="sidebarChat_info">
+        <h2>Room Name</h2>
+        <p>This is the last message</p>
+      </div>
+    </div>
+  )
+}
+
+export default SidebarChat
