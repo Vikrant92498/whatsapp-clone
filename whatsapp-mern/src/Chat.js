8af318f187129bfeb8a77a2a2d45d2fec07f9024
vikrant <vikrantkumar92498@gmail.com>
2023-05-28 16:56:59 +0530

    Complete frontend implementation of chat section

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -3,7 +3,43 @@ import './Chat.css'
 function chat() {
   return (
     <div className='chat'>
-      <h1>I am chat component</h1>
+      <div className="chat_header">
+       
+        <div className="chat_headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat_headerRight">
+
+        </div>
+        
+      </div>
+      <div className="chat_body">
+        <p className='chat_message'>
+          <span className='chat_name'>Vikrant</span>
+          This is a messgae
+          <span className='chat_timestamp'>
+            {new Date().toUTCString()}
+          </span>
+        </p>
+        <p className='chat_message chat_receiver'>
+          <span className='chat_name'>Vikrant</span>
+          This is a messgae
+          <span className='chat_timestamp'>
+            {new Date().toUTCString()}
+          </span>
+        </p>
+      </div>
+      <div className="chat_footer">
+
+        <form action="">
+          <input 
+          placeholder='Type a message'
+          type='text'
+          />
+          <button  type='submit'>Send a message</button>
+        </form>
+      </div>
     </div>
   )
 }
